e0d9ce5b2deb06f02d6e6baa44d099a89c944f1f
Hercules Shek <hercules.shek@gmail.com>
2015-09-20 14:41:10 +0800

    for

---
+++ ch1/char_io.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+void char_io_01()
+{
+  int c;
+  c = getchar();
+  while(c != EOF){
+    putchar(c);
+    c = getchar();
+  }
+}
+
+void char_io_02()
+{
+  int c;
+  while((c = getchar()) != EOF){
+    putchar(c);
+  }
+}
+// exercise 
+void char_io_03()
+{
+  int c;
+  c = getchar();
+  int flag = c != EOF;
+  while(flag){
+    printf("flag is %d\n", flag);
+    putchar(c);
+    if(c != 10){ // new line char
+      printf("\n");
+    }
+    c = getchar();
+    flag = c != EOF;
+  }
+  if(c == EOF){
+    printf("EOF is %d\n", EOF);
+  }
+}
+
+void char_num()
+{
+  long nc;
+  nc = 0;
+  while(getchar() != EOF)
+    ++nc;
+  printf("nc is %d\n", nc);
+}
+
+void char_num_2()
+{
+  double nc;
+  for(nc = 0; getchar() != EOF; ++nc)
+    ;
+  printf("nc is %.0f\n", nc);
+}
+
+void line_num()
+{
+  int c, nl;
+  nl = 0;
+  while((c = getchar()) != EOF)
+    if(c == '\n')
+      ++nl;
+  printf("line num is %d\n", nl);
+}
+
+main()
+{
+  // char_io_01();
+  // char_io_02();
+  // char_io_03();
+  // char_num();
+  // char_num_2();
+  line_num();
+}
--- ch1/for.c
+++ ch1/for.c
@@ -2,7 +2,13 @@
 
 main()
 {
+  for(int fahr = 0; fahr <= 300; fahr += 20){
+    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
+  }
 
-
-
+  printf("=====================\n");
+  
+  for(int fahr = 300; fahr >= 0; fahr -= 20){
+    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
+  }
 }
