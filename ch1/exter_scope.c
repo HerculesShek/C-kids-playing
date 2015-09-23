4d9e9cfa2c785d1935cb6e1e702ae5200e27544b
Hercules Shek <hercules.shek@gmail.com>
2015-09-23 23:12:18 +0800

    ch1 finished

---
+++ ch1/exter_scope.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+// 获取输入的所有行 将长度最大的打印出来
+#define MAXLINE 1000
+
+int max;				// 最大长度
+char line[MAXLINE];		// 当前输入行
+char longest[MAXLINE]; 	// 最长的行
+
+int get_line(void);
+void copy(void);
+
+int main()
+{
+  int len;
+  // extern int max;
+  // extern char longest[];
+
+  max = 0;
+  while((len = get_line()) > 0)
+  	if(len > max){
+  		max = len;
+  		copy();
+  	}
+  if(max>0)
+  	printf("%s", longest);
+  return 0;
+}
+
+int get_line()
+{
+	int c, i;
+	// extern char line[];
+	for(i=0;i<MAXLINE-1 && (c=getchar()) != EOF && c!='\n'; ++i)
+		line[i] = c;
+	if(c == '\n'){
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+void copy()
+{
+	int i;
+	// extern char line[], longest[];
+	i = 0;
+	while((longest[i] = line[i])!= '\0')
+		++i;
+}
--- ch1/reverse.c
+++ ch1/reverse.c
@@ -2,21 +2,50 @@
 
 #define MAXLINE 1000
 
-char[] reverse(char to[]);
-
+int get_line(char s[], int limit);
+void reverse(char to[], char from[], int len);
+void test_rev(char from[]);
 
 int main()
 {
-
+  int len;
+  char line[MAXLINE];
+  
+  while((len = get_line(line, MAXLINE)) > 0)
+    test_rev(line);
   return 0;
 }
 
 
-char[] reverse(char from[])
+void test_rev(char from[])
 {
   int len = 0;
   while(from[len] != '\0')
     ++len;
+  char to[len+1];
+  reverse(to, from, len);
+  printf("%s\n", to);
+}
+
+void reverse(char to[], char from[], int len)
+{
+  for(int i=0; i<len; ++i)
+    to[i] = from[len-i-1];
+  to[len] = '\0';
+}
 
 
+int get_line(char s[], int limit)
+{
+  int c, i;
+  for(i=0; i<limit-1 && (c=getchar()) != EOF && c!='\n'; ++i)
+    s[i] = c;
+  /*
+  if(c == '\n'){
+    s[i] = c;
+    ++i;
+  } 
+  */
+  s[i] = '\0';
+  return i;
 }
