fa9c4083d6e31bbe715c4bbc4b9a0c8f0f33c781
Hercules Shek <hercules.shek@gmail.com>
2015-10-07 18:07:55 +0800

    list dir

---
+++ ch8/8.6_example_listing_dir/dirent.h
@@ -0,0 +1,21 @@
+#ifndef DIRENT_H
+#define DIRENT_H
+
+#define NAME_MAX   14  /* longest filename component; */
+                       /* system-dependent */
+
+typedef struct {       /* portable directory entry */
+  long ino;                  /* inode number */
+  char name[NAME_MAX+1];     /* name + '\0' terminator */
+} Dirent;
+
+typedef struct { /* minimal DIR: no buffering, etc. */
+  int fd; /* file descriptor for the directory */
+  Dirent d; /* the directory entry */
+} DIR;
+
+DIR *opendir(char *dirname);
+Dirent *readdir(DIR *dfd);
+void closedir(DIR *dfd);
+
+#endif // DIRENT_H
