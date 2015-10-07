200370e4d04d70f2986a8156c723d9ff7b3e33a0
Hercules Shek <hercules.shek@gmail.com>
2015-10-07 17:11:52 +0800

    review implementation of fopen and getc

---
+++ ch8/8.5_example_fopen_getc/stdio.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "stdio.h"
+
+
+/* _fillbuf: allocate and fill input buffer */
+int _fillbuf(FILE *fp)
+{
+  int bufsize;
+  if ((fp->flag&(_READ | _EOF | _ERR)) != _READ)
+    return EOF;
+  bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZ;
+  if (fp->base == NULL)     /* no buffer yet */
+    if ((fp->base = (char *) malloc(bufsize)) == NULL)
+      return EOF;       /* can’t get buffer */
+  fp->ptr = fp->base;
+  fp->cnt = read(fp->fd, fp->ptr, bufsize);
+  if (--fp->cnt < 0) {
+    if (fp->cnt == -1)
+      fp->flag |= _EOF;
+    else
+      fp->flag |= _ERR;
+    fp->cnt = 0;
+    return EOF; }
+  return (unsigned char) *fp->ptr++;
+}
+
+
+FILE _iob[OPEN_MAX] = {    /* stdin, stdout, stderr */
+  { 0, (char *) 0, (char *) 0, _READ, 0 },
+  { 0, (char *) 0, (char *) 0, _WRITE, 1 },
+  { 0, (char *) 0, (char *) 0, _WRITE | _UNBUF, 2 }
+};
