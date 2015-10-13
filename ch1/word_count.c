f070190d5d6c04e430351e36f419e0fab81e21a0
Hercules Shek <hercules.shek@gmail.com>
2015-10-13 09:28:53 +0800

    format

--- ch1/word_count.c
+++ ch1/word_count.c
@@ -9,13 +9,13 @@ void status()
   int c, state, nc, nw, nl;
   state = OUT;
   nl = nw = nc = 0;
-  while((c = getchar()) != EOF){
+  while ((c = getchar()) != EOF) {
     ++nc;
-    if('\n' == c)
+    if ('\n' == c)
       ++nl;
-    if(' ' == c || '\n' == c || '\t' == c){
+    if (' ' == c || '\n' == c || '\t' == c) {
       state = OUT;
-    } else if(state == OUT){
+    } else if (state == OUT) {
       state = IN;
       ++nw;
     }
