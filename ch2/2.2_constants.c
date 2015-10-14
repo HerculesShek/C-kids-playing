7870237713f8b5fd4fc6029e1c5d1fb4582eeb0b
Hercules Shek <hercules.shek@gmail.com>
2015-10-14 23:44:39 +0800

    escape char

--- ch2/2.2_constants.c
+++ ch2/2.2_constants.c
@@ -3,9 +3,29 @@
 int main()
 {
   int c = '0';
-  printf("%d\n", c);
+  printf("%d\n", c); // 48
 
   c = '\0';
-  printf("%d\n", c);
+  printf("%d\n", c); // 0
+
+  c = '\n';
+  printf("%d\n", c); // 10
+  c = 'A';
+  printf("%d\n", c); // 65
+
+  c = '\12';
+  printf("%d\n", c); // 10
+  c = '\xa';
+  printf("%d\n", c); // 10
+  printf("%c", c); // '\n'
+
+
+  c = '\101';
+  printf("%c\n", c); // A
+  c = '\x41';
+  printf("%c\n", c); // A
+
+  
+  
   return 0;
 }
