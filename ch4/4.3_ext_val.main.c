584190177a146badfc046ebf1ff5083592a612e2
Hercules Shek <hercules.shek@gmail.com>
2015-10-23 23:36:43 +0800

    sign reverse polish

--- ch4/4.3_ext_val.main.c
+++ ch4/4.3_ext_val.main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h> // for atof 
+#include <stdlib.h> // for atof
+#include <math.h>
 
 #define MAXOP 100 /* max size of operand or operator */
 #define NUMBER '0' /* signal that a number was found */
+#define PI 3.14159265
+typedef enum {SIN = 1024, EXP, POW} opr;
+
 
 int getop(char []);
 void push(double);
@@ -11,7 +15,7 @@ double pop(void);
 // Reverse Polish
 int main()
 {
-  // printf("%g\n", atof("-.78"));
+  // printf("%g\n", sin(270 * PI / 180));
   // return 0;
   int type;
   double op2;    // second operator 
@@ -45,6 +49,7 @@ int main()
       else
         printf("error: zero modular\n");
       break;
+    
     case '\n':
       printf("\t%.8g\n", pop());
       break;
@@ -117,11 +122,19 @@ void clear()
 }
 // ----------- stack ---------------
 
-
+// ====================== getop ============================
 #include <ctype.h> // for isdigit
+#include <string.h> // for index 
 
 int getch(void);
 void ungetch(int);
+char *opers[] = {
+  "sin",
+  "exp",
+  "pow"
+};
+int num_opers = sizeof(opers) / sizeof(opers[0]);
+char *operator; // 如果是惨做函数 sin exp 则指向它
 
 /* getop:  get next character or numeric operand */
 int getop(char s[])
@@ -130,15 +143,34 @@ int getop(char s[])
   while ((s[0] = c = getch()) == ' ' || c == '\t')
     ;
   s[1] = '\0';
-  // 这个if是加了符号判断之后 修改的 目前来看 代码稍显丑陋 和 下文衔接不够好
-  if (!isdigit(c) && c != '.') { 
+  // 这个if是加了符号判断之后 修改的 目前来看 代码稍显丑陋 并且 和下文衔接不够好
+  if (!isdigit(c) && c != '.') {
     if (c == '-' || c == '+') { /* maybe a sign - or + */
       d = getch();
       ungetch(d);
-      if (!isdigit(d))      /* not a number */
+      if (!isdigit(d))  /* not a number but '-' or '+' */
         return c;
-    } else
-      return c;
+    } else {            /* other operatoer or word  '*' '/' 'x' 'w'... */
+      if (c == '/' || c == '*' || c == '%') /* '/' '*' or '%' */ 
+        return c;
+      else { // "xxxsinyyy" xxx and yyy will be ignored 还好没有 math-cos这种函数 否则'-'的处理就十分麻烦了 并且现在 log10 这种函数 还支持不了 TODO
+        int j = 0;
+        while (!isdigit(s[++j]=c=getch()) && c!='.' && c!='+' && c!= '-' && !isspace(c))
+          ;
+        s[j] = '\0';
+        ungetch(c);
+
+        int op_flag = is_operator(s);
+        if (op_flag == 0)
+          return operator; // TODO return enum
+        else if (op_flag > 0) { // TODO ungetch 
+          
+        } else { // TODO return enum
+          return unknow;
+        }
+        
+      }
+    }
   }
   i = 0;
   if (isdigit(c) || c == '-' || c == '+')    /* collect integer part */
@@ -153,6 +185,20 @@ int getop(char s[])
   return NUMBER;
 }
 
+int is_operator(char *op) // 看字符串op中是否含有 操作函数 比如 sin exp
+{
+  char *p = NULL;
+  for (int i=0; i<num_opers; i++) {
+    p = strstr(op, opers[i]);
+    if (p) {
+      operator = opers[i];
+      return p - opers[i];
+    }
+  }
+  return -1;
+}
+
+// ---------------------- getop ----------------------------
 
 #define BUFSIZE 100
 
